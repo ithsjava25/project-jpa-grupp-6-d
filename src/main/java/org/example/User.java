cca81bef4635ec160a21aa23b23abb2922455e7a
Linus Samuelsson <smulle.junior@gmail.com>
2025-12-16 09:54:38 +0100

    Adds user

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,62 @@
+package org.example;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    private Long userId;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String username;
+    private String password;
+
+    public User(){
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
+
