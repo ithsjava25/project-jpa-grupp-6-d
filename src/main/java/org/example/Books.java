5b249620ceaf1f80c1ff3c1d90064cd3efbf9498
Linus Samuelsson <smulle.junior@gmail.com>
2025-12-16 09:49:00 +0100

    Adds books

---
+++ src/main/java/org/example/Books.java
@@ -0,0 +1,78 @@
+package org.example;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Books {
+
+    @Id
+    private Long id;
+
+    private String author;
+    private String title;
+    private String genre;
+    private String description;
+    private int isbn;
+    private int publishYear;
+
+    public Books() {
+    }
+
+    public int getPublishYear() {
+        return publishYear;
+    }
+
+    public void setPublishYear(int publishYear) {
+        this.publishYear = publishYear;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
