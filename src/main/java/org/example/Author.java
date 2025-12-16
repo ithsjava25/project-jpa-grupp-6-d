7a5142174d71fd5c4f7ced739248343200339308
Linus Samuelsson <smulle.junior@gmail.com>
2025-12-16 10:15:54 +0100

    Adds author
    Co-authored-by: Dennis Selden <111012436+dennsel@users.noreply.github.com>
    
    Co-authored-by: Linda Eskilsson
    <eskilsson.linda@hotmail.se>
    
    Co-authored-by: Erika Falk Svendsen
    <alicia-fs@outlook.com>

---
+++ src/main/java/org/example/Author.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long authorId;
+
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Long getAuthorId() {
+        return authorId;
+    }
+
+}
