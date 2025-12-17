9f691f7e08b4b83327c0f20c89efc7af343011fa
Linus Samuelsson <smulle.junior@gmail.com>
2025-12-17 11:42:26 +0100

    Test

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,34 @@
 package org.example;
 
+import jakarta.persistence.*;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello There!");
+     static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("library_system");
+
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+
+            User user = new User();
+
+            user.setUsername("testUser");
+            user.setEmail("testMail");
+            user.setPassword("test");
+            user.setFirstName("test");
+            user.setLastName("test");
+
+            em.persist(user);
+
+
+            em.getTransaction().commit();
+
+        } finally {
+            em.close();
+            emf.close();
+        }
     }
+}
 
