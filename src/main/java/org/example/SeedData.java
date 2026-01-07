674547012650f2bdb6ebf3005f0e835307359c68
Linda Eskilsson <eskilsson.linda@hotmail.se>
2026-01-07 16:04:20 +0100

    SeedData with functions to impoprt data from tsv.

---
+++ src/main/java/org/example/SeedData.java
@@ -0,0 +1,16 @@
+package org.example;
+
+//todo:
+//1. läsa TSV-fil: seed-books.tsv
+//2. skapa Author, Book, Genre-objekt
+//3. koppla dem
+//4. köra persist() + commit() → då hamnar datan i våran DB.
+
+public class SeedData {
+
+    public static void main(String[] args) {
+        System.out.println("Seed startar...");
+
+
+    }
+}
