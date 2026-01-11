88ca577cedb143d5d2481a1708d4431a91cf0c7c
Linda Eskilsson <eskilsson.linda@hotmail.se>
2026-01-11 17:56:54 +0100

    test: add basic test of framework

--- src/test/java/org/example/BookSearchTest.java
+++ src/test/java/org/example/BookSearchTest.java
@@ -1,5 +1,14 @@
 package org.example;
 
-public class BookSearchTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class BookSearchTest {
+
+    @Test
+    void testIfFrameworkWorks() {
+        assertTrue(true);
+    }
 }
+
