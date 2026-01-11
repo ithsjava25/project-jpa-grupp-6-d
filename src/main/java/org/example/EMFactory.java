b271b793bb8bcc9df0a8218a913e3e5c48f86a61
Dennis Selden <111012436+DennSel@users.noreply.github.com>
2026-01-11 11:47:17 +0100

    Added initialization method

--- src/main/java/org/example/EMFactory.java
+++ src/main/java/org/example/EMFactory.java
@@ -7,10 +7,15 @@ import jakarta.persistence.Persistence;
 public class EMFactory {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("library_system");
 
+    public static void init(){
+        // Call this once to initialize the factory att app start
+    }
+
     // Use this in try-with
     public static EntityManager getEntityManager() {
         return emf.createEntityManager();
     }
+
 
     // Close the factory
     public static void close() {
